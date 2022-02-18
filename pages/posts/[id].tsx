df4f42f24482c9c50d022eb83f730b84ffce30af
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2022-02-18 11:08:44 -0300

    Migrate to TypeScript

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,10 +1,11 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
 import Head from 'next/head'
 import Layout from '../../components/layout'
 import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
@@ -12,8 +13,8 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string)
   return {
     props: {
       postData
